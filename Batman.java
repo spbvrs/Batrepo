32b95725a7fc4b4c5fed9412adbb4149556d73af
spbvrs <spbvrs@gmail.com>
2018-01-30 02:57:12 +0530

    added Batman

---
+++ Batman.java
@@ -0,0 +1,20 @@
+public class Batman{
+
+  public static void main(String[] args){
+
+    for(int i=0;i<=10;i++){
+
+     System.out.println("NA NA NA NA NA NA....Batman:" +i);	
+
+
+
+ }
+
+
+
+}
+ 
+
+
+
+}
